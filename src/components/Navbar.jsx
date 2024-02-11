f51022dbf8ab83c864fa6120ce89af4de0278f86
JavokhirTursunboev <jtursunboev@bk.ru>
2024-02-12 08:46:49 +0900

    Update package name and add responsive button to Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,30 @@
+import Link from "next/link";
 export default function Navbar() {
-  return <div>navbar</div>;
+  return (
+    <div className=" h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
+      {/* ==== LOGO ==== */}
+      <div>
+        <Link
+          href="/"
+          className="text-sm bg-black rounded-md p-1 font-semibold flex items-center justify-center"
+        >
+          <span className=" text-white mr-1">Javokhir</span>
+          <span className=" text-black bg-white rounded w-12 h-8 flex items-center  justify-center ">
+            .dev
+          </span>
+        </Link>
+      </div>
+
+      {/* ===== RESPONSIVE BUTTON ===================== */}
+
+      <div className="">
+        {/* MENU BUTTON */}
+        <button>
+          <div className="w-10 h-1 bg-black rounded"></div>
+          <div className="w-10 h-1 bg-black rounded"></div>
+          <div className="w-10 h-1 bg-black rounded"></div>
+        </button>
+      </div>
+    </div>
+  );
 }
