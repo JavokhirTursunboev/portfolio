bcb4a20528264c1001aff10ae73a28c057d48bf8
JavokhirTursunboev <jtursunboev@bk.ru>
2024-03-17 23:10:50 +0900

    Remove remotePatterns from images configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images:{
-        remotePatterns:[{
-            protocol:"https",
-            hostname:"images.pexels.com"
-        }]
-    }
+   
 };
 
 export default nextConfig;
